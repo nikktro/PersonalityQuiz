de71e20e157c80054537c21f147343fea964c4ab
Nikolay Trofimov <nikktro@gmail.com>
2022-02-05 11:21:02 +0300

    add classes for VCs

---
+++ PersonalityQuiz/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Nikolay Trofimov on 05.02.2022.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+}
---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Nikolay Trofimov on 05.02.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+}
