2345104b9bc73e45afec9fa8d9a6079905eed5cb
Nikolay Trofimov <nikktro@gmail.com>
2022-02-05 11:43:18 +0300

    QuestionVC IBOutlets

--- PersonalityQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionsViewController.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    // MARK: Private Properties
+    private let questions = Question.getQuestions()
+    
 }
