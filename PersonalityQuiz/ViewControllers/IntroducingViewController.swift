ea23845ce67f4ce53fa0c08e23d61972d72dcba3
Nikolay Trofimov <nikktro@gmail.com>
2022-02-05 11:17:45 +0300

    unwind segue for Done button

---
+++ PersonalityQuiz/ViewControllers/IntroducingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IntroducingViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Nikolay Trofimov on 28.01.2022.
+//
+
+import UIKit
+
+class IntroducingViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+
+}
+
