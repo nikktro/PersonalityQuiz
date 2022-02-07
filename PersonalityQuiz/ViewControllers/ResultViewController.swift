f9e78b1434889b89f40545c30f0f2e725fb01045
Nikolay Trofimov <nikktro@gmail.com>
2022-02-07 23:57:24 +0300

    ResultViewController refactoring logic start

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -20,30 +20,28 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.setHidesBackButton(true, animated: false)
+        navigationItem.hidesBackButton = true
         
-        if let resultAnimal = findResultAnimal(for: chosenAnswers) {
-            animalLabel.text = "Вы - \(resultAnimal.rawValue)"
-            animalDescriptionLabel.text = resultAnimal.definition
-        }
+
     }
     
     // MARK: Private Methods
-    private func findResultAnimal(for: [Answer]) -> Animal? {
-        var results: [Animal: Int] = [.dog: 0, .cat: 0, .rabbit: 0, .turtle: 0]
+    private func findMostFrequentAnimal() -> Animal? {
         
+        // TODO: add logic
+        var results: [Animal: Int] = [:]
         for answer in chosenAnswers {
-            switch answer.animal {
-            case .dog:
-                results[.dog]! += 1
-            case .cat:
-                results[.cat]! += 1
-            case .rabbit:
-                results[.rabbit]! += 1
-            case .turtle:
-                results[.turtle]! += 1
-            }
+            
         }
+        
+
         return results.max(by: { $0.value < $1.value })?.key
+    }
+    
+    private func UpdateUI() {
+        if let resultAnimal = findMostFrequentAnimal() {
+            animalLabel.text = "Вы - \(resultAnimal.rawValue)"
+            animalDescriptionLabel.text = resultAnimal.definition
+        }
     }
 }
